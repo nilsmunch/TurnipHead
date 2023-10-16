ef558810d4e2d7e71cfa338f3c77344a65bd6538
Nils Munch <nils.munch@lego.com>
2023-10-16 17:41:59 +0200

    Indexing fixes

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 /*
 |--------------------------------------------------------------------------
